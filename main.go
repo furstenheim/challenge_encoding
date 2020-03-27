0ab4eb3cedf47b108225e86b8d7fbc751d7416db
Gabriel <furstenheim@gmail.com>
2020-03-27 22:14:28 +0100

    Move int to function

--- main.go
+++ main.go
@@ -13,36 +13,49 @@ func Parse (t interface{}, reader io.Reader) (error) {
 	// buffedReader := bufio.NewReader(reader)
 	typeOf := reflect.TypeOf(t)
 	nFields := typeOf.NumField()
-	fields := make([]*field, nFields)
+	parser := typeParser{
+		fields: make([]field, nFields),
+		nFields: nFields,
+	}
 	for i := 0; i < nFields; i++ {
 		structField := typeOf.Field(i)
-		tag := structField.Tag
-		fmt.Println(structField)
-		fmt.Println(tag)
-		indexString, ok := tag.Lookup("index")
-		if !ok {
-			return fmt.Errorf(fmt.Sprintf("Missing index for field %s", structField.Name))
+		index, parseIndexError := parser.parseIndex(structField)
+		if parseIndexError != nil {
+			return parseIndexError
 		}
-		index, parseError := strconv.ParseInt(indexString, 10, 64)
-		if parseError != nil {
-			return fmt.Errorf(fmt.Sprintf("Index property could not be parsed for field %s", structField.Name))
-		}
-		if int(index) >= nFields {
-			return fmt.Errorf(fmt.Sprintf("Received index too big for field %s value %d", structField.Name, index))
-		}
-		if index < 0 {
-			return fmt.Errorf(fmt.Sprintf("Received negative index for field %s value %d", structField.Name, index))
-		}
-		if fields[index] != nil {
-			return fmt.Errorf(fmt.Sprintf("Received repeated index for field %s", structField.Name))
-		}
-		fields[index] = &field{
+		parser.fields[index] = field{
 			index: int(index),
+			delimiter: "",
 		}
 	}
-	log.Println(fields)
+	log.Println(parser)
 	return nil
 // 	buffedReader.ReadString('\n')
+}
+type typeParser struct {
+	fields []field
+	nFields int
+}
+func (parser typeParser) parseIndex (structField reflect.StructField) (int, error) {
+	tag := structField.Tag
+	indexString, okIndex := tag.Lookup("index")
+	if !okIndex {
+		return 0, fmt.Errorf(fmt.Sprintf("Missing index for field %s", structField.Name))
+	}
+	index, parseError := strconv.ParseInt(indexString, 10, 64)
+	if parseError != nil {
+		return 0, fmt.Errorf(fmt.Sprintf("Index property could not be parsed for field %s", structField.Name))
+	}
+	if int(index) >= parser.nFields {
+		return 0, fmt.Errorf(fmt.Sprintf("Received index too big for field %s value %d", structField.Name, index))
+	}
+	if index < 0 {
+		return 0, fmt.Errorf(fmt.Sprintf("Received negative index for field %s value %d", structField.Name, index))
+	}
+	if parser.fields[index].delimiter != "" {
+		return 0, fmt.Errorf(fmt.Sprintf("Received repeated index for field %s", structField.Name))
+	}
+	return int(index), nil
 }
 
 type field struct {
@@ -50,3 +63,6 @@ type field struct {
 	index int
 	delimiter string
 }
+
+
+
