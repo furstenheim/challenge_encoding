e7b96ead3884ea558729be1651e2c605fd65de42
Gabriel <furstenheim@gmail.com>
2020-03-27 22:23:20 +0100

    Parse delimiter

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"reflect"
 	"strconv"
 )
-const SPACE_DELIMITER = "space"
 
 func Parse (t interface{}, reader io.Reader) (error) {
 	// buffedReader := bufio.NewReader(reader)
@@ -23,9 +22,13 @@ func Parse (t interface{}, reader io.Reader) (error) {
 		if parseIndexError != nil {
 			return parseIndexError
 		}
+		delimiter, delimiterError := parser.parseDelimiter(structField)
+		if delimiterError != nil {
+			return delimiterError
+		}
 		parser.fields[index] = field{
 			index: int(index),
-			delimiter: "",
+			delimiter: delimiter,
 		}
 	}
 	log.Println(parser)
@@ -36,24 +39,37 @@ type typeParser struct {
 	fields []field
 	nFields int
 }
+
+func (parser typeParser) parseDelimiter (structField reflect.StructField) (string, error) {
+	tag := structField.Tag
+	delimiterText, ok := tag.Lookup("delimiter")
+	if !ok {
+		return "\n", nil
+	}
+	if delimiterText == "space" {
+		return " ", nil
+	}
+	return "", fmt.Errorf("unknown delimiter %s", delimiterText)
+}
+
 func (parser typeParser) parseIndex (structField reflect.StructField) (int, error) {
 	tag := structField.Tag
 	indexString, okIndex := tag.Lookup("index")
 	if !okIndex {
-		return 0, fmt.Errorf(fmt.Sprintf("Missing index for field %s", structField.Name))
+		return 0, fmt.Errorf("missing index for field %s", structField.Name)
 	}
 	index, parseError := strconv.ParseInt(indexString, 10, 64)
 	if parseError != nil {
-		return 0, fmt.Errorf(fmt.Sprintf("Index property could not be parsed for field %s", structField.Name))
+		return 0, fmt.Errorf("index property could not be parsed for field %s", structField.Name)
 	}
 	if int(index) >= parser.nFields {
-		return 0, fmt.Errorf(fmt.Sprintf("Received index too big for field %s value %d", structField.Name, index))
+		return 0, fmt.Errorf("received index too big for field %s value %d", structField.Name, index)
 	}
 	if index < 0 {
-		return 0, fmt.Errorf(fmt.Sprintf("Received negative index for field %s value %d", structField.Name, index))
+		return 0, fmt.Errorf("received negative index for field %s value %d", structField.Name, index)
 	}
 	if parser.fields[index].delimiter != "" {
-		return 0, fmt.Errorf(fmt.Sprintf("Received repeated index for field %s", structField.Name))
+		return 0, fmt.Errorf("received repeated index for field %s", structField.Name)
 	}
 	return int(index), nil
 }
